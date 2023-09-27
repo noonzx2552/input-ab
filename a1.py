e9d70f6cc33be6a83653245f69617ffb2c3e5792
noonzx2552 <noonzx2552@gmail.com>
2023-09-27 19:35:24 +0700

    first commit

---
+++ a1.py
@@ -0,0 +1,7 @@
+a = str(input("วันนี้ฝนตกมั้ย[Y/N]:"))
+if a == "Y":
+    print("ฉันจะขับรถไปห้างเซน")
+elif a == "N":
+    print("ฉันจะขี่มอเตอร์ไซไปตลาดวัดลุ่ม")
+else: 
+    print("data incorrect pls type again")   
